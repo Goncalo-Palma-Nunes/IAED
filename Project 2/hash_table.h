2c00aba7d5aaaa482387bd2718252d12f1e19060
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2021-05-17 17:17:17 +0100

    Header file for hash table struct

---
+++ Project 2/hash_table.h
@@ -0,0 +1,31 @@
+/*
+ * File: hash_table.h
+ * Author: Gonçalo Palma Ribeiro Graça Nunes
+ * Description: Header file for a hash table structure. In this implementation,
+ * the table is a table of TreeNode structs
+*/
+
+/* Constants */
+
+#define M 100000  /* Size of the table */
+
+/* Function Prototypes */
+
+/* Hash function for the table. Receives a pointer to char and the size of
+ * the table and returns an index on the table */
+int hash(char *v);
+
+/* Initializes a hash table and returns a pointer to the start of the table */
+TreeNode** STinit();
+
+/* Receives an integer representation of the key to an item and deletes
+ * the first item found with that key */
+void STdelete(unsigned long id, TreeNode **st)
+
+/* Receives a pointer to a TreeNode to a hash table and inserts the TreeNode
+ * on the table */
+void STinsert(TreeNode *node, TreeNode **st);
+
+/* Receives a pointer to a hash table and returns the first element found
+ * with the value v */
+TreeNode* STsearch(char *v, TreeNode **st);
